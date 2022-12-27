a62ca92b2c882767e05f1a6b02002bc93bd4b1b4
João Victor Ribeiro <joao.leiterib19@gmail.com>
2022-12-27 20:19:37 -0300

    Adicionando comentários

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+// Dados Iniciais
 let currentQ = 0;
 let correctAnswer = 0;
 let p = 0;
 let progress = 0;
 showQuestion();
 
+// Evento de click
+// Evento para fazer o botão de "Fazer Novamente" funcionar
 document.querySelector('.button').addEventListener('click', restart);
 
-
+// Funções
 function showQuestion(){
+    // Matemática para calcular a porcentagem de acertos e a movimentação da barra de progresso
     p = Math.floor( (correctAnswer / questions.length) * 100 );
     progress = Math.floor((currentQ /questions.length) * 100);
 
-
+    // Codigo para mostrar as questões e as opções presentes no Quiz, assim como o resultado ao final do questionário
     if(questions[currentQ]){
     
         let quest = questions[currentQ];
@@ -45,6 +49,7 @@ function showQuestion(){
     }
 };
 
+// Função para validar a escolha das opções e, também, armazenar se acertou ou não a questão
 function selectOp(e){
         let numberOptions= parseInt(e.target.getAttribute('data-option'));
         
@@ -56,6 +61,7 @@ function selectOp(e){
         showQuestion();
 };
 
+// Função que mostra o quadro de resultado
 function showResult(){
 
     if(p <30){
@@ -76,6 +82,7 @@ function showResult(){
     };
 };
 
+// Função para começar o Quiz novamente
 function restart(){
     currentQ = 0;
     correctAnswer = 0;
